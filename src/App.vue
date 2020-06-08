a298fd9d143faa410e3d1d1d4d9881dd51bf9ca1
skynix <romain.day.ludovic@gmail.com>
2020-06-08 17:35:02 +0200

    ui login page

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <router-view /> 
+  <v-app>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
@@ -7,3 +11,7 @@ export default {
   name: 'App',
 };
 </script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,56 @@
 <template>
-  <v-container fluid>
+  <v-container fill-height fluid class="login-container">
       <v-row>
-          <v-col cols="12">
+          <v-col cols="12" sm="8" offset="2">
+              <p style="text-align: center">Ici image</p>
+          </v-col>
+          <v-col cols="12" sm="8" offset="2">
+              <p class="heading-login">Contaminate</p>
+          </v-col>
+          <v-col cols="12" sm="6" offset="3">
+              <v-card outlined>
+                  <v-container>
+                      <v-row>
+                          <v-col cols="12" sm="12">
+                              <p class="heading-form">Connecte toi !</p>
+                          </v-col>
+                          <v-col cols="12" sm="8" offset="2">
+                              <v-text-field
+                                outlined
+                                clearable
+                                color="#2E7D32"
+                                prepend-inner-icon="mdi-order-alphabetical-ascending"
+                                label="Pseudo"
+                                placeholder="Pseudo"
+                                :rules="[rules.required, rules.minUsername]"
+                                v-model="username"
+                              ></v-text-field>
+                          </v-col>
+                          <v-col cols="12" sm="8" offset="2">
+                              <v-text-field
+                                outlined
+                                color="#2E7D32"
+                                prepend-inner-icon="mdi-lock"
+                                label="Mot de passe"
+                                placeholder="Mot de passe"
+                                :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                                @click:append="showPassword = !showPassword"
+                                :type="showPassword ? 'text' : 'password'"
+                                :rules="[rules.required, rules.minPassword]"
+                                v-model="password"
+                              ></v-text-field>
+                          </v-col>
+                          <v-col cols="12" sm="8" offset="2">
+                              <v-btn block color="#2E7D32" dark large depressed>
+                                  Se connecter
+                              </v-btn>
+                          </v-col>
+                          <v-col cols="12" sm="6" offset="3">
+                              <span class="text-footing-form">Nouveau sur Contaminate ? </span><router-link to="/register" class="text-link-register">Inscrit-toi</router-link>
+                          </v-col>
+                      </v-row>
+                  </v-container>
+              </v-card>
           </v-col>
       </v-row>
   </v-container>
@@ -9,10 +58,42 @@
 
 <script>
 export default {
-    name:"Login"
+    name:"Login",
+    data() {
+        return {
+            rules: {
+                required: value => !!value || "Ce champs est requis",
+                minPassword: value => value.length >= 6,
+                minUsername: value => value.length >= 3
+            },
+            showError: false,
+            showPassword: false,
+            password: "",
+            username: ""
+        }
+    }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+    .heading-login{
+        text-align: center;
+        font-size: 1.5em;
+        text-transform: uppercase;
+        color: white;
+    }
+    .login-container{
+        background-color: #2E7D32;
+    }
+    .heading-form {
+        text-align: center;
+        font-size: 1.5em;
+        color: #2E7D32;
+    }
+    .text-footing-form{
+        color: #4a5568;
+    }
+    .text-link-register{
+        color: #2E7D32;
+    }
 </style>
