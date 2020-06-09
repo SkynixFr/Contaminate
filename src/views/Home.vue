564ff7ee038a9b380c9bcd73700c1fc85e30f2d6
skynix <romain.day.ludovic@gmail.com>
2020-06-09 23:44:37 +0200

    update home, testing request + token

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    {{ token }}
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-}
+  data() {
+    return {
+      token: this.$store.state.memberToken,
+    };
+  },
+  methods: {
+    test() {
+      axios
+        .get("https://contaminateapi.herokuapp.com/users", {
+          headers: {
+            "auth-token": this.token,
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.log(error.response);
+        });
+    },
+  },
+  mounted() {
+    this.test();
+  },
+};
 </script>
